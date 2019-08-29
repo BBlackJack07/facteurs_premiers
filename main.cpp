ec8883ecbbbe1af16b4a4c1f5a9a1a93f56799d4
bblackjack07 <bastien.brunel.bb@gmail.com>
2019-08-29 20:31:57 +0200

    Delete gpl.md

--- main.cpp
+++ main.cpp
@@ -1,21 +1,3 @@
-/*
-    This program is called 'Prime Numbers'. It can be used to decompose positive integers in prime factors.
-    Copyright (C) 2018  BBlackJack07
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>
-*/
-
 #include <iostream>
 #include "prime_numbers.hpp"
 
